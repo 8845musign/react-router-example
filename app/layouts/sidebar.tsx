c41b6007b320c5a7025da907bba9fa54c36acba8
8845musign <8845musign@gmail.com>
2025-07-27 18:52:17 +0900

    Sidebar: Add search functionality with query handling in loader and input
    Signed-off-by: 8845musign <8845musign@gmail.com>

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,17 +1,24 @@
 import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
+import { useEffect, useState } from "react";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+  const { contacts, q } = loaderData;
   const navigation = useNavigation();
+
+  const [query, setQuery] = useState(q || "");
+
+  useEffect(() => {
+    setQuery(q || "");
+  }, [q]);
 
   return (
     <>
@@ -27,6 +34,8 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
               name="q"
               placeholder="Search"
               type="search"
+              onChange={(e) => setQuery(e.currentTarget.value)}
+              defaultValue={query}
             />
             <div aria-hidden hidden={true} id="search-spinner" />
           </Form>
